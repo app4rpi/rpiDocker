8eae1b3c360e27b623d55c0e4835ba6e2762a89a
app4rpi <41761034+app4rpi@users.noreply.github.com>
2019-03-04 10:39:54 +0100

    Update context.sh

--- context.sh
+++ context.sh
@@ -3,11 +3,11 @@
 # chmod +x ./context.sh
 #  ---------------------------------------------------------
 if [ "$EUID" -ne 0 ]; then echo "Must be root"; exit; fi
-export RELEASE=stretch
+export RELEASE=$(lsb_release -cs)
 #
-export mainDomain=""
-export mainIP=""
-export mainColor='#DC443A'
+export mainDomain="''"
+export mainIP="''"
+export mainColor="#BA0020"
 #
 export appFolder="/app/nginx"
 export backupFolder="/app/backup"
@@ -19,12 +19,6 @@ export errorStyleLocal=false
 color=('#DC443A' '#98243A' '#11589F' '#715138' '#D69C2F' '#616247' '#898E88' '#2e5090' '#5F4B8B' '#BA0020' '#0E3A53' '#DC443A' '#98243A' '#11589F' '#715138' '#D69C2F' '#616247' '#898E88' '#2e5090' '#5F4B8B')
 #
 export context=("(domain IP workDir nameSite colorSite subDirIn)"
-"( '' html mainSite '#DC443A')"
-"(sonmel. '' sonmel sonmelSite '#98243A' mainSite)"
-"(iot. '' iot iotSite '#11589F' mainSite)"
-"(blog. '' blog blogSite '#715138' mainSite)"
-"(node. '' node nodeSite '#D69C2F' mainSite)"
-"(test. '' test testSite '#616247' mainSite)"
 ) 
 #
 export dockerNginxImage="app2linux/nginx2ssl:latest"
@@ -33,5 +27,5 @@ export SSL=""
 export SSLemail=""
 export SSLdomains=""
 export DAVconfig=""
-export verifiedContext=true
+export verifiedContext=false
 export folderCreated=false
