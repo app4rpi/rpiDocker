0949e0d32eb51a7c0251abd421c31ad977a941db
app4rpi <41761034+app4rpi@users.noreply.github.com>
2019-03-10 11:57:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ file="/etc/resolv.conf"
 echo -e "\n DNS servers\n"$LINE$LINE
 nameServers=("8.8.8.8" "1.1.1.1" "9.9.9.9" "208.67.222.222" "8.8.4.4" "149.112.112.112" "208.67.220.220")
 for name in ${nameServers[*]} ; do
-    [[ ! $(sed -n "/^nameserver $name/p;q" $file) ]] && echo 'nameserver '$name >> $file
+    [[ ! $(sed "/^nameserver $name/p;q" $file) ]] && echo 'nameserver '$name >> $file
     done
 cat $file
 echo $LINE$LINE
