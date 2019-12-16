9ff709f62131ba68b51097e879cacbe062d349c1
canostra <aferrer@canostra.eu>
2019-12-16 09:57:57 +0100

    setup-ampliacio01

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script has been tested on Raspbian 10 Buster image
+# This script has been tested on Raspbian 10 Buster image (v. September 2019)
 # chmod +x ./setup.sh
 # wget https://raw.githubusercontent.com/app4rpi/rpiDocker/master/setup.sh
 #  ---------------------------------------------------------
@@ -71,6 +71,34 @@ echo -e "\n"$LINE$LINE
 echo -en "\t"; read -rsn1 -p "Press key to continue -> " key
 echo
 return
+}
+#  ---------------------------------------------------------
+function installFirewall(){
+echo -e $LINE "\nInstall && config ufw Firewall ... "
+[[ $(dpkg --get-selections ufw) ]] && { echo "Already installed";  return 1;}
+apt-get install -y ufw
+ufw --force enable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw status verbose
+echo 'fw installed'
+echo -e $LINE "\n"
+return 1
+}
+#  ----------------------------------------
+function installOther(){
+echo -e $LINE "\nInstall git ... "
+[[ $(dpkg --get-selections git) ]] && echo "Git Already installed" || apt-get install -y git
+echo 'Install davfs2 ... '
+[[ $(dpkg --get-selections davfs2) ]] && echo "davfs2 installed" || apt-get -y install davfs2
+echo 'Install tar ... '
+[[ $(dpkg --get-selections tar) ]] && echo "tar installed" || apt-get -y install tar
+echo 'Install curl ... '
+[[ $(dpkg --get-selections curl) ]] && echo "curl installed" || apt-get -y install curl
+return 1
 }
 #  ------------------------------------------------------------------------------------------------------------------
 # Main process
