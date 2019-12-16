a70e67b7b94fc09143d9ebd4ce439bed73b3afa3
canostra <aferrer@canostra.eu>
2019-12-16 17:08:06 +0100

    setup-complet

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script has been tested on Raspbian 10 Buster image (v. September 2019)
-# chmod +x ./setup.sh
+# sudo chmod +x ./setup.sh
 # wget https://raw.githubusercontent.com/app4rpi/rpiDocker/master/setup.sh
 #  ---------------------------------------------------------
 if [ "$EUID" -ne 0 ]; then echo "Must be root"; exit; fi
@@ -20,13 +20,13 @@ return 1
 # Initial issues
 function initialIsues(){
 [ ! -d ./.startup ] && mkdir -p ./.startup
-apt autoremove --purge && apt clean
 cd ./.startup
 }
 #  -----------------------------
 function finalIssues(){
 # final issues
 echo -e "\n\t\tFinal issues...\n"
+apt -y autoremove --purge && apt -y clean
 cd ..
 }
 #  ---------------------------------------------------------
@@ -68,7 +68,6 @@ for ((i=0; i<${#file[@]}; i++)); do
     chmod +x ./${file[i]}
     done
 echo -e "\n"$LINE$LINE
-echo -en "\t"; read -rsn1 -p "Press key to continue -> " key
 echo
 return
 }
@@ -93,11 +92,24 @@ function installOther(){
 echo -e $LINE "\nInstall git ... "
 [[ $(dpkg --get-selections git) ]] && echo "Git Already installed" || apt-get install -y git
 echo 'Install davfs2 ... '
-[[ $(dpkg --get-selections davfs2) ]] && echo "davfs2 installed" || apt-get -y install davfs2
+[[ $(dpkg --get-selections davfs2) ]] && echo "davfs2 installed" || DEBIAN_FRONTEND=noninteractive apt-get -yq install davfs2
 echo 'Install tar ... '
-[[ $(dpkg --get-selections tar) ]] && echo "tar installed" || apt-get -y install tar
+[[ $(dpkg --get-selections tar) ]] && echo "tar installed" || apt-get install -y tar
 echo 'Install curl ... '
-[[ $(dpkg --get-selections curl) ]] && echo "curl installed" || apt-get -y install curl
+[[ $(dpkg --get-selections curl) ]] && echo "curl installed" || apt-get install -y curl
+return 1
+}
+#  ----------------------------------
+function installDocker(){
+echo -e $LINE "\nInstall docker ... "
+[[ $(dpkg --get-selections docker-ce) ]] && { echo "Already installed";  return 1;}
+curl -fsSL https://get.docker.com | sh
+usermod -aG docker $USER
+apt-get install -y libffi-dev libssl-dev
+apt-get install -y python python-pip
+apt-get remove -y python-configparser
+systemctl enable docker
+systemctl start docker
 return 1
 }
 #  ------------------------------------------------------------------------------------------------------------------
@@ -106,14 +118,14 @@ return 1
 clear 
 echo -e "\nAutomatic install & config & start server and web server.\n"$LINE$LINE
 #  -----------------------
-configFunctions=(initialIsues updateNameservers updateServer downloadGit ./setupServer.sh finalIssues)
+configFunctions=(initialIsues updateNameservers updateServer downloadGit installFirewall installOther installDocker finalIssues)
 for process in ${configFunctions[*]} ; do
     $process
     done
 #  ---------------------------------------------------------
 # Modify setup.sh file (this file)
-sed -i '8,$d' setup.sh
-cat <<'EOF' >> setup.sh
+sed -i '9,$d' setup.sh
+cat >> setup.sh << EOF
 cd .startup
 ./start.sh
 cd ..
